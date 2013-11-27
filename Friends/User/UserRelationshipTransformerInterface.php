3e44c257127837aecf9579c47faa9b5baf8e3ac9
michiel <boeckaert@gmail.com>
2013-11-27 07:16:10 +0100

    added userrelationship transformer interface

---
+++ Friends/User/UserRelationshipTransformerInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+* This file is part of the MilioooFriendsBundle package.
+*
+* (c) Michiel boeckaert <boeckaert@gmail.com>
+* This source file is subject to the MIT license that is bundled
+* with this source code in the file LICENSE.
+*/
+
+namespace Miliooo\Friends\User;
+
+/**
+ * Interface UserRelationshipTransformerInterface
+ *
+ * @author Michiel Boeckaert <boeckaert@gmail.com>
+ */
+interface UserRelationshipTransformerInterface
+{
+    /**
+     * Transforms the user relationship id to an UserRelationshipInterface object.
+     *
+     * @param mixed $userRelationshipId
+     *
+     * @return UserRelationshipInterface;
+     */
+    public function transformToObject($userRelationshipId);
+}
